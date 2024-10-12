ef9195f93321f29f6b5a48cb55612009a0cad4ba
George <icannotp@gmail.com>
2024-10-12 23:26:09 +0100

    updated assignment

---
+++ wwwroot/js/TaxCalculator.js
@@ -0,0 +1,8 @@
+﻿const incomeInput = document.getElementById("income");
+    incomeInput.addEventListener("input", function (e) {
+        let value = e.target.value.replace(/,/g, ''); // Remove existing commas
+    if (!isNaN(value) && value) {
+        e.target.value = Number(value).toLocaleString(); // Add commas
+        }
+    });
+
